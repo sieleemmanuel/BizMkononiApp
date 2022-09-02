09130d74e810f98bac9cd4fc26ea27696b0aea9e
kimsiele <sielekim.emmanuel@gmail.com>
2022-09-02 16:51:00 +0300

    code update

--- app/src/main/java/com/siele/mkononiapptest/ui/SalesInsights.kt
+++ app/src/main/java/com/siele/mkononiapptest/ui/SalesInsights.kt
@@ -1,14 +1,17 @@
 package com.siele.mkononiapptest.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.siele.mkononiapptest.MainActivity
+import com.siele.mkononiapptest.R
 import com.siele.mkononiapptest.databinding.FragmentSalesInsightsBinding
 import com.siele.mkononiapptest.interfaces.DrawerClosed
 import com.siele.mkononiapptest.interfaces.DrawerAndBarsLocker
+import org.eazegraph.lib.models.PieModel
 
 
 class SalesInsights : Fragment() {
@@ -26,6 +29,17 @@ class SalesInsights : Fragment() {
             handleSideNavClicks()
             updateBottomNav()
             handleBottomNavActions()
+        }
+        binding.apply {
+            pieChart.apply {
+                addPieSlice(PieModel("Roses",55F,resources.getColor(R.color.roses_color, null)))
+                addPieSlice(PieModel("Daffoiles",20F,resources.getColor(R.color.daffoiles_color, null)))
+                addPieSlice(PieModel("Lilies",29F,resources.getColor(R.color.lilies_color, null)))
+                addPieSlice(PieModel("Daises",9F,resources.getColor(R.color.daises_color, null)))
+                startAnimation()
+                innerValueString = "55%"
+            }
+
         }
         return binding.root
     }
